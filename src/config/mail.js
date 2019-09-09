656d2d5407d6064c2a63d2b66edc79a6f1624043
charlesmarafao <charlesmarafao@gmail.com>
2019-09-09 22:31:59 +0000

    feat: add email configs and send mail function

---
+++ src/config/mail.js
@@ -0,0 +1,19 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '75f521fd01b583',
+    pass: '9912d86c5ac5ce',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
+
+// Amazon SES
+// Mailgun
+// Sparkpostm
+// Mandril
+// Gmail
+
+// MailTrap
